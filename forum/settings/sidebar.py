910eeb2582a5fd0a2228a836da8f0d4fcbf9dde8
jcook <zealcook@gmail.com>
2015-02-02 19:13:13 -0800

    1. update basic setting. 2. update signin html - remove openID. 3. update base content (H5)

--- forum/settings/sidebar.py
+++ forum/settings/sidebar.py
@@ -59,15 +59,8 @@ required=False))
 
 SIDEBAR_UPPER_TEXT = Setting('SIDEBAR_UPPER_TEXT',
 u"""
-[![WebFaction logo][2]][1]
-## [Reliable OSQA Hosting][1]
-
-We recommend [**WebFaction**][1] for OSQA hosting. For \
-under $10/month their reliable servers get the job done. See our \
-[**step-by-step setup guide**](http://wiki.osqa.net/display/docs/Installing+OSQA+on+WebFaction).
-
-[1]: http://www.webfaction.com?affiliate=osqa
-[2]: /m/default/media/images/webfaction.png""", SIDEBAR_SET, dict(
+Intent Keeping Blank **A** area.
+""", SIDEBAR_SET, dict(
 label = "Upper Block Content",
 help_text = " The upper sidebar block. ",
 widget=Textarea(attrs={'rows': '10'})))
@@ -91,11 +84,7 @@ required=False))
 
 SIDEBAR_LOWER_TEXT = Setting('SIDEBAR_LOWER_TEXT',
 u"""
-## Learn more about OSQA
-
-The [**OSQA website**](http://www.osqa.net/) and [**OSQA wiki**](http://wiki.osqa.net/) \
-are great resources to help you learn more about the OSQA open source Q&A system. \
-[**Join the OSQA chat!**](http://meta.osqa.net/question/79/is-there-an-online-chat-room-or-irc-channel-for-osqa#302)
+Intent Keeping Blank **B** area.
 """, SIDEBAR_SET, dict(
 label = "Lower Block Content",
 help_text = " The lower sidebar block. ",
